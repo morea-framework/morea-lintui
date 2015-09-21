87e078dfeef55d3621d12eec317ec0a01d05ee1b
henricasanova <henric@hawaii.edu>
2015-09-21 12:11:41 -1000

    More packaging stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+
 setup(name='morea-lintui',
       version='0.1',
       description='A Morea content validator and editor',
       url='https://github.com/morea-framework/morea-lintui',
       author='Henri Casanova',
+      scripts=['morea-lintui/morea-lintui'],
       author_email='henricasanova@gmail.com',
-      license='GPL 2', packages=['morea-lintui'],
+      license='GPL 2',
+      packages=['morea-lintui'],
       zip_safe=False,
-      install_requires=['urwid', 'PyYaml'])
+      install_requires=['urwid', 'PyYaml'],
+      include_package_data=True)
