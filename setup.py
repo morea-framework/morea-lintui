147f692da31fe45d94f4d8a41da67ad659dce5b8
henricasanova <henric@hawaii.edu>
2015-09-21 13:28:57 -1000

    remove offending hyphen in package name!!

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='morea-lintui',
+setup(name='morealintui',
       version='0.1',
       description='A Morea content validator and editor',
       url='https://github.com/morea-framework/morea-lintui',
       author='Henri Casanova',
       entry_points={
-          'console_scripts': ['morea-lintui=morea-lintui.morea-lintui:main']
+          'console_scripts': ['morealintui = morealintui.morealintui:main']
       },
       author_email='henricasanova@gmail.com',
       license='GPL 2',
-      packages=['morea-lintui'],
+      packages=['morealintui'],
       zip_safe=False,
       install_requires=['urwid', 'PyYaml'],
       include_package_data=True)
