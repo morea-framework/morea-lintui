b55e0a2d623290a4972630ec8d3a7769708e926b
henricasanova <henric@hawaii.edu>
2015-09-21 11:54:30 -1000

    Re-org of the repo in a view to Python packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'casanova'
+
+from setuptools import setup
+
+setup(name='morea-lintui',
+      version='0.1',
+      description='A Morea content validator and editor',
+      url='https://github.com/morea-framework/morea-lintui',
+      author='Henri Casanova',
+      author_email='henricasanova@gmail.com',
+      license='GPL 2',
+      packages=['morea-lintui'],
+      zip_safe=False)
