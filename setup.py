5418619524b76ad0603b38022868e4dd93487d3c
henricasanova <henric@hawaii.edu>
2015-09-21 12:49:47 -1000

    typos-- (due to IntelliJ refactoring)

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='morea-lintui.py',
+setup(name='morea-lintui',
       version='0.1',
       description='A Morea content validator and editor',
-      url='https://github.com/morea-framework/morea-lintui.py',
+      url='https://github.com/morea-framework/morea-lintui',
       author='Henri Casanova',
       entry_points={
-            'console_scripts': ['morea-lintui.py=morea-lintui.py/morea-lintui.py.py:main']
+          'console_scripts': ['morea-lintui=morea-lintui/morea-lintui:main']
       },
       author_email='henricasanova@gmail.com',
       license='GPL 2',
-      packages=['morea-lintui.py'],
+      packages=['morea-lintui'],
       zip_safe=False,
       install_requires=['urwid', 'PyYaml'],
       include_package_data=True)
