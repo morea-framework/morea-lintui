6369436487f990ed33f8b5ac86d42c5c76d9e370
henricasanova <henric@hawaii.edu>
2015-09-21 15:22:20 -1000

    making progress toward a working package

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='morealintui',
       version='0.1',
       description='A Morea content validator and editor',
       url='https://github.com/morea-framework/morea-lintui',
       author='Henri Casanova',
-#      py_modules = ['morealintui.morealintui'],
+      packages=find_packages(exclude=['build','dist']),
+      #      py_modules = ['morealintui.morealintui'],
       entry_points={
           'console_scripts': ['morea-lintui = morealintui.morealintui:main'],
       },
       author_email='henricasanova@gmail.com',
       license='GPL 2',
-      packages=['morealintui'],
       zip_safe=False,
       install_requires=['urwid', 'PyYaml'],
       include_package_data=True)
