118e2e3a702ac8990807d2b1d6c6079c18716dd6
henricasanova <henric@hawaii.edu>
2015-09-21 20:00:46 -1000

    Added a hyphen, which fixes at least the "entry point not found" problem

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='morealintui',
+setup(name='morea-lintui',
       version='0.1',
       description='A Morea content validator and editor',
       url='https://github.com/morea-framework/morea-lintui',
